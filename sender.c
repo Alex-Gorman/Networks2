1f1d751eba1d4b651b5a0ba60b8cb5984fded94a
ajg029 <a.gorman029@gmail.com>
2023-02-27 13:44:20 -0600

    UDP connection setup

--- sender.c
+++ sender.c
@@ -1,29 +1,68 @@
+/*
+** talker.c -- a datagram "client" demo
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 
-int main(int args, char *input[]) {
+#define SERVERPORT "4950"	// the port users will be connecting to
 
-    /* Command line argumnets = 
-        1. host name of the receiver 
-        2. port number of the receiver 
-        3. maximum window sending size
-        4. time value in seconds that should be used to trigger re-transmissions
-    */
+int main(int argc, char *argv[])
+{
+	int sockfd;
+	struct addrinfo hints, *servinfo, *p;
+	int rv;
+	int numbytes;
 
-   /* Check for the correct number of arguments */
-   if (args != 5) {
-        printf("Incorrect number of arguments. Please enter 5 arguments. \n");
-        exit(1);
-   }
+	if (argc != 3) {
+		fprintf(stderr,"usage: talker hostname message\n");
+		exit(1);
+	}
 
-   printf(input[0]);
-   
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_INET6; // set to AF_INET to use IPv4
+	hints.ai_socktype = SOCK_DGRAM;
 
+    if ((rv = getaddrinfo(NULL, SERVERPORT, &hints, &servinfo)) != 0) {
+	// if ((rv = getaddrinfo(argv[1], SERVERPORT, &hints, &servinfo)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		return 1;
+	}
 
+	// loop through all the results and make a socket
+	for(p = servinfo; p != NULL; p = p->ai_next) {
+		if ((sockfd = socket(p->ai_family, p->ai_socktype,
+				p->ai_protocol)) == -1) {
+			perror("talker: socket");
+			continue;
+		}
 
+		break;
+	}
 
+	if (p == NULL) {
+		fprintf(stderr, "talker: failed to create socket\n");
+		return 2;
+	}
 
+	if ((numbytes = sendto(sockfd, argv[2], strlen(argv[2]), 0,
+			 p->ai_addr, p->ai_addrlen)) == -1) {
+		perror("talker: sendto");
+		exit(1);
+	}
 
+	freeaddrinfo(servinfo);
 
-    return 0;
+	printf("talker: sent %d bytes to %s\n", numbytes, argv[1]);
+	close(sockfd);
+
+	return 0;
 }
